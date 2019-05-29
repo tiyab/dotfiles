405d9c2df936b494601a603714ec3432337da0b7
Stephane DAMOUR <stef.dams@gmail.com>
2019-05-29 17:07:26 +0200

    small updates

--- install.sh
+++ install.sh
@@ -27,27 +27,27 @@ main() {
 }
 
 function sudo_get_password() {
-    echo "== Prompting for sudo password"
+    echo "==> Prompting for sudo password"
     if sudo --validate; then
         # Keep-alive
         while true; do sudo --non-interactive true; \
             sleep 10; kill -0 "$$" || exit; done 2>/dev/null &
-        echo "== Sudo password updated"
+        echo "==> Sudo password updated"
     else
-        echo "== Sudo password update failed"
+        echo "==> Sudo password update failed"
         exit 1
     fi
 }
 
 function git_make_dir() {
-  echo "== Creating Git directory"
+  echo "==> Creating Git directory"
   mkdir -p "${GITDIR}"
 }
 
 function git_clone_project() {
-  echo "== Cloning dotfiles project locally"
+  echo "==> Cloning dotfiles project locally"
   if [[ -d "${PROJECTDIR}" ]]; then
-    echo "== dotfiles already exists; checking for updates"
+    echo "==> dotfiles already exists; checking for updates"
     git -C "${PROJECTDIR}" pull
   else
     git clone "${PROJECTURL}" "${PROJECTDIR}" --branch v2
@@ -56,11 +56,11 @@ function git_clone_project() {
 
 function homebrew_install() {
   if [[ "${OSTYPE}" == darwin* ]]; then
-    echo "== Installing Homebrew"
+    echo "==> Installing Homebrew"
     if ! command -v brew &>/dev/null; then
       # Unattended installation of Homebrew
       if ! CI=1 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"; then
-        echo "== Homebrew installation failed, the installation script may be unavailable..."
+        echo "==> Homebrew installation failed, the installation script may be unavailable..."
         exit 1
       fi
     fi
@@ -68,32 +68,32 @@ function homebrew_install() {
 }
 
 function homebrew_install_brewfile() {
-  echo "== Installing tap/formulas/casks from Brewfile"
+  echo "==> Installing tap/formulas/casks from Brewfile"
   if /usr/local/bin/brew bundle install --file="${BREWFILE}"; then
-    echo "== All tap/formulas/casks from Brewfile are installed"
+    echo "==> All tap/formulas/casks from Brewfile are installed"
   else
-    echo "== Failed to install tap/formulas/casks from Brewfile"
+    echo "==> Failed to install tap/formulas/casks from Brewfile"
   fi
 }
 
 function shell_set_default() {
   if command -v zsh &>/dev/null; then
     if [[ "$SHELL" != *zsh* ]]; then
-      echo "== Setting up zsh as default shell for ${USER}"
+      echo "==> Setting up zsh as default shell for ${USER}"
       sudo chsh -s /usr/local/bin/zsh "${USER}"
       if [[ $(dscl . -read /Users/"${USER}" UserShell) == "UserShell: /usr/local/bin/zsh" ]]; then
-        echo "== zsh is now the default shell"
+        echo "==> zsh is now the default shell"
       else
-        echo "== Failed to set zsh as default shell"
+        echo "==> Failed to set zsh as default shell"
       fi
     fi
   fi
 }
 
 function shell_set_prezto() {
-  echo "== Setting up prezto for zsh"
+  echo "==> Setting up prezto for zsh"
   if [[ -d "${HOME}/.zprezto" ]]; then
-    echo "== prezto already install, running update"
+    echo "==> prezto already install, running update"
     git -C "${HOME}/.zprezto" pull
   else
     git clone --recursive https://github.com/sorin-ionescu/prezto.git "${HOME}/.zprezto"
@@ -101,79 +101,79 @@ function shell_set_prezto() {
   shopt -s extglob
   # shellcheck disable=SC2154
   if [[ -d ${PROJECTDIR}/files/zsh ]]; then
-    echo "== Setting up custom zsh configuration"
+    echo "==> Setting up custom zsh configuration"
     find "${PROJECTDIR}/files/zsh" -type f -name 'z*' -exec sh -c 'name=$(basename ${1}); ln -sf ${1} ${HOME}/.${name}' _ {} \;
     ln -sfF "${PROJECTDIR}/files/zsh/zsh" "${HOME}/.zsh"
   else
-    echo "== Setting up default prezto configuration"
+    echo "==> Setting up default prezto configuration"
     find "${HOME}/.zprezto/runcoms" -type f -name 'z*' -exec sh -c 'name=$(basename ${1}); ln -sf ${1} ${HOME}/.${name}' _ {} \;
   fi
-  echo "== prezto for zsh has been setup"
+  echo "==> prezto for zsh has been setup"
   shopt -u extglob
 }
 
 function sshkey_config() {
   if [[ -f ${HOME}/.ssh/id_rsa && -f ${HOME}/.ssh/id_rsa.pub ]]; then
-    echo "== Existing SSH keys detected, skipping SSH keys creation"
+    echo "==> Existing SSH keys detected, skipping SSH keys creation"
   else
-    echo "== Generating new ssh key"
+    echo "==> Generating new ssh key"
     ssh-keygen -t rsa -C "${USER}@$HOSTNAME" -q -N '' -f "${HOME}/.ssh/id_rsa"
-    echo "== ssh key available in ${HOME}/.ssh"
+    echo "==> ssh key available in ${HOME}/.ssh"
   fi
 }
 
 function hostsfile_update() {
-  echo "== Updating /etc/hosts from someonewhocares.org"
+  echo "==> Updating /etc/hosts from someonewhocares.org"
   if sudo curl -s -o /etc/hosts https://someonewhocares.org/hosts/hosts; then
-    echo "== /etc/hosts has been successfully updated"
+    echo "==> /etc/hosts has been successfully updated"
   else
-    echo "== Failed to update /etc/hosts"
+    echo "==> Failed to update /etc/hosts"
   fi
 }
 
 function vim_install_vundle() {
-  echo "== Vundle installation"
+  echo "==> Vundle installation"
   if command -v vim &>/dev/null; then
     if [[ -d "${HOME}/.vim/bundle/Vundle.vim" ]]; then
-      echo "== Updating Vundle"
+      echo "==> Updating Vundle"
       git -C "${HOME}/.vim/bundle/Vundle.vim" pull
     else
-      echo "== Installing Vundle"
+      echo "==> Installing Vundle"
       git clone https://github.com/VundleVim/Vundle.vim.git "${HOME}/.vim/bundle/Vundle.vim"
       if [[ -f ${PROJECTDIR}/files/vimrc ]]; then
-        echo "== Setting up custom vimrc"
+        echo "==> Setting up custom vimrc"
         ln -sf "${PROJECTDIR}/files/vimrc" "${HOME}/.vimrc"
-        echo "== Installing plugins"
+        echo "==> Installing plugins"
         # https://github.com/VundleVim/Vundle.vim/issues/511#issuecomment-134251209
         echo | vim +PluginInstall +qall &>/dev/null
         # vim +PluginInstall +qall
       fi
     fi
   else
-    echo "== Vim not installed"
+    echo "==> Vim not installed"
   fi
 }
 
 function vscode_set_config() {
-  echo "== Setting up settings for VSCode"
+  echo "==> Setting up settings for VSCode"
   if [[ -f ${FILES}/vscode/settings.json ]]; then
     cp -a "${FILES}/vscode/settings.json" "${HOME}/Library/Application\ Support/Code/User/settings.json"
     if [[ $(md5 -q "${FILES}"/vscode/settings.json) != $(md5 -q "${HOME}"/Library/Application\ Support/Code/User/settings.json) ]]; then
-      echo "== Failed to copy VSCode settings"
+      echo "==> Failed to copy VSCode settings"
     fi
   fi
-  echo "== Setting up keybindings for VSCode"
+  echo "==> Setting up keybindings for VSCode"
   if [[ -f ${FILES}/vscode/keybindings.json ]]; then
     cp -a "${FILES}/vscode/keybindings.json" "${HOME}/Library/Application\ Support/Code/User/keybindings.json"
     if [[ "$(md5 -q "${FILES}"/vscode/keybindings.json)" != "$(md5 -q "${HOME}"/Library/Application\ Support/Code/User/keybindings.json)" ]]; then
-      echo "== Failed to copy VSCode keybindings"
+      echo "==> Failed to copy VSCode keybindings"
     fi
   fi
 }
 
 function os_customize() {
   if [[ ${OSTYPE} == darwin* ]]; then
-    echo "== Customizing MacOS"
+    echo "==> Customizing MacOS"
     "${PROJECTDIR}"/macos.sh "${PROJECTDIR}"
   fi
 }
